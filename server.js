c960c0a9978a67d1b92eaed24845fb50c3b78ae0
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-06-11 09:41:48 -0400

    wrote listening server and basic route

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-require('dotenv')
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT
 
 
+app.get('/',(req,res)=>{
+    res.send(`Welcome to my drop of hope on the World Wide Web`)
+})
+
+app.listen(PORT,()=>{
+    console.log(`your server is listening on ${PORT}`)
+})
