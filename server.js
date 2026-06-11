8158c17352bb02a5c1a4101be76f6bf079137555
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-06-11 16:25:48 -0400

    commented out api request

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.get('/about',(req,res)=>{
     res.send(`This project started with passion for looking at our ever changing world and wanting to contribute a peace of hope but awareness to people as they traverse this beautiful earth we have been given. It was also important for me to highlight the different cultures and cultural norms you could face. I don't believe that we are all meant to be the same person but I believe in the basic principal of respect and that is for all marginaled people. So my solution was to create a space that allowed for information and community to be fostered so we can feel safe no matter where we go on this earth that was gifted to ALL species`)
 })
 
+
+//get all countries route
 app.get('/api/countries', async (req, res)=>{
 
     try {
@@ -27,12 +29,45 @@ app.get('/api/countries', async (req, res)=>{
 
         const data = await apiResponse.json()
         console.log(data)
-        const transformedData = data.data.objects.map(country => ({
+        const transformedCountriesData = data.data.objects.map(country => ({
             name: country.names.common,
             population: country.population
         }))
 
-        res.json(transformedData)
+        res.json(transformedCountriesData)
+
+    } catch (error) {
+        console.error('Error fetching or transforming users:', error);
+        res.status(500).send(`Countries not found`)
+    }
+
+})
+
+app.get('/api/countries', async (req, res)=>{
+
+    try {
+
+        const countryName = req.query.q || 'nothing'
+
+        // let apiResponse = await fetch(
+        //     `https://api.restcountries.com/countries/v5?q=${countryName}&limit=5`,
+        //     {headers: { 'Authorization': APIKEY }}
+        // )
+
+        if(!apiResponse.ok){
+            throw new Error(`apiResponse was not ok ${apiResponse.status}`)
+        }
+
+        const data = await apiResponse.json()
+        console.log(data)
+
+        // const transformedCountriesData = data.data.objects.map(country => ({
+        //     id: country.uuid,
+        //     name: country.names.common,
+        //     population: country.population
+        // }))
+
+        res.json(data)
 
     } catch (error) {
         console.error('Error fetching or transforming users:', error);
