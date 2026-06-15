b68d0344790d50e74991d2a94f5f182401e59add
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-06-15 19:25:10 -0400

    created another route to return one user. succesfully returned user

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: {type: String, required: true},
-    password: {type: String, required: true},
+    username: { type: String, required: true, unique: true, trim: true, minlength: 10 },
+    password: { type: String, required: true, trim: true, minlength: 5 },
 });
 
 const User = mongoose.model("User", userSchema);
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -12,14 +12,21 @@ usersRoutes.get('/api/users', (req, res) => {
 
     res.send('booooooo')
 })
-usersRoutes.get('/api/user/:id', async (req, res) => {
+usersRoutes.get('/api/user/:username', async (req, res) => {
+
+    const specificUser = await User.findOne({ 'username': req.params.username })
+
     console.log(` user routes hit`)
-    res.send(`users routes are here`)
+    res.send(`users routes are here`, specificUser)
 })
+
+
 usersRoutes.post('/api/user/', async (req, res) => {
 
     User.create(req.body).then(createdUser => {
         console.log(`User ${createdUser.username} was created`)
+    }).catch(error => {
+        console.log(error, `<<<<<<<<===== ERROR HERE HELP!!`)
     })
 
     res.send(req.body)
