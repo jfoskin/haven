cc037a6e2e63f4eb09a20b969aeef0b091bf21c4
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-06-15 17:57:09 -0400

    included app.use middleware and created a test user to send data to database

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -3,19 +3,29 @@
 const express = require('express')
 const usersRoutes = express.Router()
 
+const User = require('../models/UserModel')
+
 //Routes
 
-usersRoutes.get('/api/users', (req,res)=>{
+usersRoutes.get('/api/users', (req, res) => {
     console.log(`hey`)
 
     res.send('booooooo')
 })
-usersRoutes.get('/api/user/:id', async (req,res)=>{
+usersRoutes.get('/api/user/:id', async (req, res) => {
     console.log(` user routes hit`)
     res.send(`users routes are here`)
 })
+usersRoutes.post('/api/user/', async (req, res) => {
 
-usersRoutes.post('/api/users', (req,res)=>{
+    User.create(req.body).then(createdUser => {
+        console.log(`User ${createdUser.username} was created`)
+    })
+
+    res.send(req.body)
+})
+
+usersRoutes.post('/api/users', (req, res) => {
     console.log(`hey yall don't stop`)
 })
 
--- server.js
+++ server.js
@@ -26,52 +26,54 @@ mongoose.connect(uri)
 
 const db = mongoose.connection
 
-db.on('error', (error)=> console.log(error.message,`Database has an error`))
-db.on('connected', ()=> console.log(`Database has successfully connected`))
-db.on('disconnected', ()=> console.log(`Database has disconnected`))
+db.on('error', (error) => console.log(error.message, `Database has an error`))
+db.on('connected', () => console.log(`Database has successfully connected`))
+db.on('disconnected', () => console.log(`Database has disconnected`))
 
 //Middleware
 
+app.use(express.urlencoded({ extende: true }))
+
 const client = new MongoClient(uri)
 
-async function mongodbCOnnection () {
+async function mongodbCOnnection() {
     try {
         await client.connect()
 
         console.log(`Database connection is succesful`)
     } catch (error) {
-        console.log(`AN ERROR has OCCURED while trying to connect to MongoDB... an error HAS occured`,error)
+        console.log(`AN ERROR has OCCURED while trying to connect to MongoDB... an error HAS occured`, error)
     }
 }
 
-countriesClient.interceptors.request.use(config =>{
-console.log(`sending headers hopefully`)
+countriesClient.interceptors.request.use(config => {
+    console.log(`sending headers hopefully`)
 
     return config
 },
- error => {
-  // Handle request error
-  return Promise.reject(error);
-})
+    error => {
+        // Handle request error
+        return Promise.reject(error);
+    })
 
 //Routes
- 
-app.get('/',(req,res)=>{
+
+app.get('/', (req, res) => {
     res.send(`Welcome to my drop of hope on the World Wide Web`)
 })
 
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
     res.send(`This project started with passion for looking at our ever changing world and wanting to contribute a peace of hope but awareness to people as they traverse this beautiful earth we have been given. It was also important for me to highlight the different cultures and cultural norms you could face. I don't believe that we are all meant to be the same person but I believe in the basic principal of respect and that is for all marginaled people. So my solution was to create a space that allowed for information and community to be fostered so we can feel safe no matter where we go on this earth that was gifted to ALL species`)
 })
 
-app.post('')
+// app.post('/')
 
 app.use('/', countriesRoutes)
 app.use('/', usersRoutes)
 
 //Ports
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`your server is listening on ${PORT}`)
     mongodbCOnnection()
 })
